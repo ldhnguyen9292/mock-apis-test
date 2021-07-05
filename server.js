732e2fe1240751a272e7ef2eef3b331d9ef85224
ldhnguyen9292 <ldhnguyen9292@gmail.com>
2021-07-05 17:15:35 +0700

    update database

--- server.js
+++ server.js
@@ -7,25 +7,3 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 server.listen(port);
-
-var db = require("./db.json");
-server.get("/get/carousel", (req, res) => {
-  let id = req.query["id"];
-  if (id != null && id >= 0) {
-    let result = db.carouselList.find((carousel) => {
-      return carousel.id == id;
-    });
-    if (result) {
-      let { id, ...carousel } = result;
-      res.status(200).jsonp(carousel);
-    } else {
-      res.status(400).jsonp({
-        error: "Bad userId",
-      });
-    }
-  } else {
-    res.status(400).jsonp({
-      error: "No valid userId",
-    });
-  }
-});
